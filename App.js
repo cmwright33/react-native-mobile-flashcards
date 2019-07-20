69be893dc9b064d86248027129bd59a966dd6295
Chris Wright <cmwright33@gmail.com>
2019-07-20 14:53:40 -0400

    change adddeck to adddeck screen build tab navigation through react nav

--- App.js
+++ App.js
@@ -1,83 +1,34 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { createBottomTabNavigator , createAppContainer , Platform } from 'react-navigation';
-import AddDeck from './components/AddDeck.js'
+import AddDeckScreen from './components/AddDeckScreen.js'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <AppContainer/>
-    </View>
-  );
+function HomeScreen( ){
+  return (<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text> Home </Text>
+    </View>)
 }
 
 
-const Tabs = createBottomTabNavigator({
+
+function TestScreen( ){
+  return (<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text> Test! </Text>
+    </View>)
+}
+
+const AppNavigator = createBottomTabNavigator({
   Home: {
-    screen: Home,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    },
+    screen: HomeScreen
   },
-  Test: {
-    screen: Test,
-    navigationOptions: {
-      tabBarLabel: 'Test',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    },
-  },
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor:  '#FFF',
-    style: {
-      height: 56,
-      backgroundColor: '#FEFE',
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
+  AddDeck: {
+    screen: AddDeckScreen
   }
-})
-
-
-const AppContainer = createAppContainer(Tabs);
-
-function Home( ){
-  return (<View>
-      <Text> Helloooo </Text>
-    </View>)
-}
-
-
-
-function Test( ){
-  return (<View>
-      <Text> Helloooo </Text>
-    </View>)
-}
-
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
+
+export default createAppContainer(AppNavigator);
+
+
--- components/AddDeckScreen.js
+++ components/AddDeckScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, TextInput } from 'react-native';
 
 
-class AddDeck extends Component {
+class AddDeckScreen extends Component {
 
   state = {
     text: ''
@@ -11,7 +11,7 @@ class AddDeck extends Component {
   render(){
     
     return(
-      <View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text> add entry </Text>
         <TextInput
          style={{height: 100}}
@@ -25,4 +25,4 @@ class AddDeck extends Component {
 }
 
 
-export default AddDeck;
+export default AddDeckScreen;
