f9bbf22648af28c994253fc352c4330083cce9ee
C Wright <cmwright33@gmail.com>
2019-07-19 23:50:34 -0400

    start adding views

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Home/>
       <Text>Open up App.js to start working on your app!</Text>
+      <AddDeck/>
     </View>
   );
 }
+
+
+function Home( ){
+  return (<View>
+      <Text> Helloooo </Text>
+    </View>)
+}
+
+
+function AddDeck( ){
+  return (<View>
+      <Text> add entry </Text>
+      <TextInput
+       placeholder="Name the Deck!"
+      />
+    </View>)
+}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
